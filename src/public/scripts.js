3bd5d4dfc6746badd959944822b3091a6264202f
Chris Jaure <chrisjaure@gmail.com>
2013-07-29 19:42:09 -0700

    style improvements

--- src/public/scripts.js
+++ src/public/scripts.js
@@ -3,6 +3,6 @@
 jQuery.noConflict();
 jQuery(function($) {
 	$('.allure-component').on('click', '.allure-component-code-toggle', function(e) {
-		$(e.delegateTarget).find('.allure-component-code').toggle();
+		$(e.delegateTarget).toggleClass('allure-component-toggled');
 	});
 });
